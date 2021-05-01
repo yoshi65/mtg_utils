66e671f6387c416afd23a82d228089c22355f8a2
Yoshitaka Yamazaki <yoshi65.ymzk@gmail.com>
2021-05-01 12:53:37 +0900

    fixed name

--- InsertCardInfo/lambda_function.py
+++ InsertCardInfo/lambda_function.py
@@ -3,16 +3,16 @@
 #
 # FileName: 	lambda_function
 # CreatedDate:  2021-04-27 20:41:27 +0900
-# LastModified: 2021-05-01 12:33:38 +0900
+# LastModified: 2021-05-01 12:52:54 +0900
 #
 
 
 import json
 import boto3
+from bs4 import BeautifulSoup
 import logging
 from os import getenv
 import requests
-import time
 from urllib.parse import parse_qs
 
 logger = logging.getLogger()
@@ -30,32 +30,34 @@ def lambda_handler(event, context):
     table = resource.Table(table_name)
 
     url = generate_url(name)
+    site = requests.get(url)
+    both_name = get_both_name(site)  # ja_name/en_name
 
-    if is_correct_url(name, url):
-        insert_card_info(table, name, url)
+    if is_correct_url(name, site):
+        insert_card_info(table, name, both_name, url)
         payload = {
             'attachments': [{
                 'color': '#D3D3D3',
-                'pretext': f'Inserted {name} to {table_name}',
+                'pretext': f'Inserted {both_name} to {table_name}',
             }]
         }
         post_slack(payload)
         return {
             "statusCode": 200,
-            "body": json.dumps({"message": f"Successfully insert {name} to {table_name}", }),
+            "body": json.dumps({"message": f"Successfully insert {both_name} to {table_name}", }),
         }
     else:
         payload = {
             'attachments': [{
                 'color': '#EB3228',
-                'pretext': f'Failed to insert {name} to {table_name}',
+                'pretext': f'Failed to insert {both_name} to {table_name}',
             }]
         }
         post_slack(payload)
         return {
             "statusCode": 400,
             "body": json.dumps({
-                "message": f"Fail to insert {name} to {table_name} because generated url is not correct",
+                "message": f"Fail to insert {both_name} to {table_name} because generated url is not correct",
             }),
         }
 
@@ -74,21 +76,24 @@ def translate_name(name: str) -> str:
     return name.replace(' ', '+').replace(',', '%2C')
 
 
-def insert_card_info(table, name: str, url: str) -> None:
+def insert_card_info(table, name: str, both_name: str, url: str) -> None:
     table.put_item(
         Item={
             'Name': name,
             'URL': url,
+            'both_name': both_name,
             'Prices': dict()
         }
     )
 
 
-def is_correct_url(name: str, url: str) -> bool:
-    site = requests.get(url)
-    time.sleep(1)
-
+def is_correct_url(name: str, site) -> bool:
     return name in site.text
+
+
+def get_both_name(site) -> str:
+    data = BeautifulSoup(site.text, "html.parser")
+    return data.select('h1.wg-title')[0].get_text()
 
 
 def post_slack(payload):
