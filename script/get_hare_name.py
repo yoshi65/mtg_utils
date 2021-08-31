a62cec5d89abb42fa53631c34b35c42a245f7d56
Yoshitaka Yamazaki <yoshi65.ymzk@gmail.com>
2021-08-31 19:09:33 +0900

    added get_hare_name

--- script/get_hare_name.py
+++ script/get_hare_name.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# FileName: 	tmp
+# FileName: 	get_hare_name
 # CreatedDate:  2021-05-16 15:44:46 +0900
-# LastModified: 2021-05-16 15:46:09 +0900
+# LastModified: 2021-08-31 19:09:06 +0900
 #
 
+import sys
 import requests
 from bs4 import BeautifulSoup
 
 
-url = "https://www.hareruyamtg.com/ja/products/detail/69767?lang=EN"
+url = sys.argv[1]
 site = requests.get(url)
 data = BeautifulSoup(site.text, "html.parser")
 title = data.select("title")[0].get_text().split('|')[0]
